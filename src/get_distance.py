9d17b8350f420054c242cea678a37f736c54858f
brucec <brucec@hotmail.com>
2019-06-28 13:22:08 -0700

    add feature to publish depth map as np.array in gazebo

--- src/get_distance.py
+++ src/get_distance.py
@@ -14,15 +14,28 @@ import numpy as np
 import time
 import pickle
 
+pub_depth = rospy.Publisher("depth", numpy_msg(Floats), queue_size=10)
 def listener():
     def callback(cloud):
-        print("call back entered")
-        bridge=CvBridge()
+        bridge = CvBridge()
         cv_image = bridge.imgmsg_to_cv2(cloud, desired_encoding="passthrough")
-        cv2.imshow("rgb",cv_image)
-        cv2.waitKey(100)
-        time.sleep(3)
-        print (cv_image.shape)
+        img_copy = np.copy(np.float32(cv_image))
+        if img_copy.shape == (256, 256):
+
+            inds = np.where(np.isnan(img_copy))
+
+            MAX_DEPTH = 10
+            img_copy[inds] = MAX_DEPTH
+            pub_depth.publish(img_copy.ravel())
+            
+            # pickle.dump(foo, open("foonan.p", "wb"))
+            # pickle.dump( foo, open( "nofoonan.p", "wb" ) )
+            # print(np.amax(foo))
+
+            # print(type(foo))
+            # print('foo was saved')
+            # sub.unregister()
+            # print('sub was unregistered')
 
         # gen = pc2.read_points(cloud, skip_nans=True, field_names=("x", "y", "z"))
         # print(sum(1 for x in gen))
@@ -31,10 +44,9 @@ def listener():
         # for val in gen:
         #     #print(type(val))
         #     print(val.shape)
-        
+
         # for p in pc2.read_points(cloud, field_names = ("x", "y", "z"), skip_nans=True):
         #     print " x : %f  y: %f  z: %f" %(p[0],p[1],p[2])
-
 
         # cloud_points = list(
         #     pc2.read_points(cloud, skip_nans=False, field_names=("x", "y", "z"))
@@ -43,10 +55,9 @@ def listener():
         # print('saved')
         # sub.unregister()
 
-
     print("listener started")
     rospy.init_node("depth_distance")
-    #sub = rospy.Subscriber("/camera/depth/points", PointCloud2, callback)
+    # sub = rospy.Subscriber("/camera/depth/points", PointCloud2, callback)
     sub = rospy.Subscriber("/camera/depth/image_raw", Image, callback)
     rospy.spin()
 
