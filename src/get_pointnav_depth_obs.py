038d2f238f0f20ec3e9f758b6d657eedc355d036
brucec <brucec@hotmail.com>
2019-07-16 19:07:25 -0700

    refactor get observation and pointnav goal

--- src/get_pointnav_depth_obs.py
+++ src/get_pointnav_depth_obs.py
@@ -6,62 +6,62 @@ from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import PointCloud2
-import sensor_msgs.point_cloud2 as pc2
 
 import tf
 import math
 
 import cv2
 import numpy as np
-import time
-import pickle
 
-# class NN_sensor():
-#     def __init__(self):
-#         self.depth=None
-#         self.goal=None
 
 pub_depth_and_pointgoal = rospy.Publisher(
     "depth_and_pointgoal", numpy_msg(Floats), queue_size=10
 )
-linear = None
-angular = None
+
+
+DEPTH_IMG_WIDTH = 256
+DEPTH_IMG_HEIGHT = 256
+MAX_DEPTH = 10
+FAKE_DISTANCE_AMOUNT = 0.2
 
 
 def listener():
-    global linear
-    global angular
+    def callback(img):
+        try:
+            (trans, rot) = tf_listner.lookupTransform(
+                "/base_footprint", "/goal", rospy.Time(0)
+            )
+        except (
+            tf.LookupException,
+            tf.ConnectivityException,
+            tf.ExtrapolationException,
+        ):
+            pass
 
-    def callback(cloud):
-        print('call back entered')
+        angular = math.atan2(trans[1], trans[0])
+        linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+        print("call back entered")
 
-        bridge = CvBridge()
-        cv_image = bridge.imgmsg_to_cv2(cloud, desired_encoding="passthrough")
+        cv_image = CvBridge().imgmsg_to_cv2(img, desired_encoding="passthrough")
         img_copy = np.copy(np.float32(cv_image))
 
         print("inside call back linear is " + str(linear))
         print("inside call back angular is " + str(angular))
-        if img_copy.shape == (256, 256):
-
+        if img_copy.shape == (DEPTH_IMG_HEIGHT, DEPTH_IMG_WIDTH):
             inds = np.where(np.isnan(img_copy))
-
-            MAX_DEPTH = 10
             img_copy[inds] = MAX_DEPTH
-            img_copy = (img_copy-0.2)/10
-            pickle.dump( img_copy, open( "foo.p", "wb" ) )
+
+            # /10 from scaling factor of Habitat implmentation
+            img_copy = (img_copy - FAKE_DISTANCE_AMOUNT) / 10
+
             depth_np = np.float32(img_copy.ravel())
             pointgoal_np = np.float32([linear, angular])
             depth_pointgoal_np = np.concatenate((depth_np, pointgoal_np))
             pub_depth_and_pointgoal.publish(np.float32(depth_pointgoal_np))
 
-            # cv2.imshow("Depth", img_copy)
-            # cv2.waitKey(100)
-            # time.sleep(0.2)
-
     rospy.init_node("depth_distance")
-    # sub = rospy.Subscriber("/camera/depth/points", PointCloud2, callback)
-
+    linear = None
+    angular = None
     rate = rospy.Rate(10.0)
     tf_listner = tf.TransformListener()
 
@@ -74,33 +74,12 @@ def listener():
         except:
             pass
 
-    print("trans is initialized")
-    (trans, rot) = tf_listner.lookupTransform("/base_footprint", "/goal", rospy.Time(0))
-
     angular = math.atan2(trans[1], trans[0])
     linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-    print(linear)
 
-    sub = rospy.Subscriber("/camera/depth/image_raw", Image, callback)
+    rospy.Subscriber("/camera/depth/image_raw", Image, callback)
 
-    while not rospy.is_shutdown():
-        try:
-            (trans, rot) = tf_listner.lookupTransform(
-                "/base_footprint", "/goal", rospy.Time(0)
-            )
-        except (
-            tf.LookupException,
-            tf.ConnectivityException,
-            tf.ExtrapolationException,
-        ):
-            continue
-
-        angular = math.atan2(trans[1], trans[0])
-        linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-        # print("linear is " + str(linear))
-        # print("angular is " + str(angular))
-
-        rate.sleep()
+    rospy.spin()
 
 
 if __name__ == "__main__":
